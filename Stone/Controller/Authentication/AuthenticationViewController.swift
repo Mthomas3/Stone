260e674fa948f85395dc402bd5fd02c692bc811d
Thomas Martins <thomas.martins@epitech.eu>
2019-05-30 19:12:31 +0200

    starting authentication

--- Stone/Controller/Authentication/AuthenticationViewController.swift
+++ Stone/Controller/Authentication/AuthenticationViewController.swift
@@ -8,11 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class AuthenticationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
 
---
+++ Stone/Tools/UITools/StringToUIColor.swift
@@ -0,0 +1,32 @@
+//
+//  StringToUIColor.swift
+//  Stone
+//
+//  Created by Thomas Martins on 30/05/2019.
+//  Copyright © 2019 Thomas Martins. All rights reserved.
+//
+
+import UIKit
+
+final class StringToUIColor {
+    
+    static func getUiColor(hexString: String) -> UIColor {
+        var cString: String = hexString.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if cString.hasPrefix("#") {
+            cString.remove(at: cString.startIndex)
+        }
+        if (cString.count) != 6 {
+            return UIColor.gray
+        }
+        var rgbValue: UInt32 = 0
+        Scanner(string: cString).scanHexInt32(&rgbValue)
+        
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+}
